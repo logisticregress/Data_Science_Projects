0da768e037c4a38b67774fd2b14a795eaea1b880
logisticregress <43940666+logisticregress@users.noreply.github.com>
2020-08-06 18:24:54 -0700

    Create Bayesian_Forecast.py

---
+++ Bayesian_Forecast.py
@@ -0,0 +1,51 @@
+def bayes_forecast(data, brand, Y, prior, var_names = [], forecast_start=None, forecast_end=None, discount=.98, return_all=False, X=None, 
+        seasPeriods=[52], seasHarmComponents=[[1,2]], nsamps=3000, ntrend=2):
+    data = data.copy()
+    data = data[data['Brand Name'] == brand]
+    start = 0
+    Y = data[Y].values[start:]
+    if not var_names: 
+        X = None
+    else:
+        X = data[var_names].values[start:]
+    family = 'normal'
+    prior_length = prior
+    if forecast_start is None: forecast_start = 0
+    if forecast_end is None: forecast_end = len(Y)-1
+    s0 = 0.01
+    
+    if ntrend == 1:
+        deltrend = discount
+    elif ntrend == 2:
+        deltrend = [discount, 0.97]
+     
+    dates = data.Date[start:]
+    date = pd.to_datetime(data.Date).dt.date
+    k = 1
+    ## Run the analysis
+    mod, samples = analysis(Y, X=X,
+    family=family,
+    forecast_start=forecast_start,
+    forecast_end=forecast_end,
+    seasPeriods=seasPeriods,
+    nsamps=nsamps,
+    seasHarmComponents=seasHarmComponents,
+    ntrend=ntrend,
+    k=k,
+    prior_length=prior_length,
+    deltrend=deltrend,
+    delregn=discount,
+    delVar=discount,
+    s0=s0)
+    
+    # set confidence interval
+    credible_interval=95
+    alpha = (100-credible_interval)/2
+    upper=np.percentile(samples, [100-alpha], axis=0).reshape(-1)
+    lower=np.percentile(samples, [alpha], axis=0).reshape(-1)
+    
+    ## If return_all=True, return a lot of information
+    if return_all:
+        return mod, samples, Y[forecast_start:forecast_end+1], date, prior_length, lower, upper
+
+    forecast = median(samples)
